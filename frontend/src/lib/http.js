95e1ca5c59c535da433e867bb9f3eecf970a561c
yusufakbs <akbasyusuuf@gmail.com>
2024-05-30 01:07:55 +0300

    Token have been added to request

--- frontend/src/lib/http.js
+++ frontend/src/lib/http.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 import {i18nIntance} from "../locales/index.js";
+import {loadToken, storeToken} from "../shared/state/storage.js";
 
 const http = axios.create();
 
+let authToken = loadToken();
+
+export function setToken(token){
+    authToken = token;
+    storeToken(token);
+}
+
 http.interceptors.request.use(config => {
     config.headers["Accept-Language"] = i18nIntance.language;
+    if(authToken){
+        config.headers["Authorization"] = `${authToken.prefix} ${authToken.token}`;
+
+    }
     return config;
 })
 
