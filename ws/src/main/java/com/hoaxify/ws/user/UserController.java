0de64ad04030aef6a7181c51d9b562bd63c57152
yusufakbs <akbasyusuuf@gmail.com>
2024-05-31 02:53:45 +0300

    @PutMapping UserDTO, refactored with method level authorization

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -8,12 +8,12 @@ import com.hoaxify.ws.user.dto.UserDTO;
 import com.hoaxify.ws.shared.GenericMessage;
 import com.hoaxify.ws.shared.Messages;
 import com.hoaxify.ws.user.dto.UserUpdate;
-import com.hoaxify.ws.user.exception.AuthorizationException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -53,10 +53,8 @@ public class UserController {
     }
 
     @PutMapping("/api/v1/users/{id}")
-    UserDTO updateUser(@PathVariable long id, @Valid @RequestBody UserUpdate userUpdate, @AuthenticationPrincipal CurrentUser currentUser) {
-        if (currentUser.getId() != id) {
-            throw new AuthorizationException();
-        }
+    @PreAuthorize("#id == principal.id")
+    UserDTO updateUser(@PathVariable long id, @Valid @RequestBody UserUpdate userUpdate ){
         return new UserDTO(userService.userUpdate(id, userUpdate));
     }
 
